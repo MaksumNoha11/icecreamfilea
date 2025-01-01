73344c5c6d4aafeef65535787e342328439925f0
MaksumNoha11 <noha.nastya@gmail.com>
2025-01-01 16:11:33 +0200

    Add files via upload

---
+++ img/js.js
@@ -0,0 +1,10 @@
+person=prompt ("Ваше ім'я");
+alert ('Hello, world! '+ person+"!");
+confirm ("Ви хочете продовжити так/ні?");
+years=prompt ("Скільки вам років?", 15);
+confirm ("Вам дійсно "+years+" років?");
+let a = person;
+let c = years;
+document.write ("   Привіт   ",  a  )
+document.write ("   Тобі     ",   c ,"   Років")
+
---
+++ img/lesson16.js
@@ -0,0 +1,3 @@
+var dol = prompt("Доллари",100)
+dolraxynok = dol * 41;
+document.write("грн="+ dolraxynok);
